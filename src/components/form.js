a39a2e6240f015b8334de6a2be387831f1fa97ae
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-04-05 11:06:30 -0700

    change button color

--- src/components/form.js
+++ src/components/form.js
@@ -7,12 +7,20 @@ flex-flow: row;
 justify-content: center;
 `;
 
+let Button = styled.button`
+background: white;
+height: 20px;
+padding: 0 10px;
+color: black;
+`;
+
+
 const Form = (props) => {
     return (
         <FormDiv>
 	  <form onSubmit={props.loadWeather}>
             <input type="text" name="city" placeholder="City" />
-            <button>Get Weather</button>
+            <Button>Get Weather</Button>
 	  </form>
 	</FormDiv>
     );
